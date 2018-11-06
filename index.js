f513fb6afd294e89b57c15bcd5bad6298d6f1c98
Cheng Vue <cvu3@ucsc.edu>
2018-11-06 18:15:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// add solution here
+function theBeatlesPPlay(MusicianArr, instArr) {
+  var resultArr = [];
+
+  for (var i = 0; i < musicianArr.length; i++) {
+    var musician = musicianArr[i];
+    var instrument = instArr[i];
+    var str = `${musician} plays ${instrument}`;
+
+    resultArr.push(str);
+  }
+
+  return resultArr;
+}
